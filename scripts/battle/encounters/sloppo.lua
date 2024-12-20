62d9fb206a750a336a8aba115df38a90b542a152
hyperboid <23138363+polypoyo@users.noreply.github.com>
2024-12-19 23:54:16 -0500

    fix BG

--- scripts/battle/encounters/sloppo.lua
+++ scripts/battle/encounters/sloppo.lua
@@ -4,30 +4,18 @@ function Dummy:init()
     super.init(self)
 
     -- Text displayed at the bottom of the screen at the start of the encounter
-    self.text = "* The tutorial begins...?"
+    self.text = "* The [style:GONER][color:red][speed:0.5][font:main_mono,48]Tutoriel[font:reset][speed:1][style:reset][color:reset] begins"
 
     -- Battle music ("battle" is rude buster)
     self.music = "booty_boss.loop"
     -- Enables the purple grid battle background
-    self.background = false
+    self.background = true
 
     -- Add the dummy enemy to the encounter
     self:addEnemy("sloppo")
 
     --- Uncomment this line to add another!
     --self:addEnemy("dummy")
-end
-
-function Dummy:drawBackground(fade)
-    local scale_fac = 16
-    local canvas = Draw.pushCanvas(SCREEN_WIDTH/scale_fac, SCREEN_HEIGHT/scale_fac)
-    Draw.setColor({.8,.8,.8}, fade)
-    Draw.rectangle("fill", 0,0,SCREEN_WIDTH/scale_fac, SCREEN_HEIGHT/scale_fac)
-    love.graphics.setShader()
-    Draw.popCanvas()
-    Draw.setColor(COLORS.white, fade)
-    Draw.draw(canvas,0,0,0,scale_fac,scale_fac)
-    return false
 end
 
 return Dummy
